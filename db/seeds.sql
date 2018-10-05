6d357adf80573f4927b90d8b6a642bad601f854b
holland cookson <hollandcookson@hollands-MacBook-Pro-2.local>
2018-10-05 12:59:42 -0400

    updated file structure

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (name, devoured) VALUES ('cheeseburger', true);
+INSERT INTO burgers (name, devoured) VALUES ('baconburger', false);
+INSERT INTO burgers (name, devoured) VALUES ('doublecheeseburger', true);
